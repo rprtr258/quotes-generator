2c2ec1b5be5d93e51c8148e5664db4f499759b26
rprtr258 <poletansky@gmail.com>
2021-10-12 04:26:09 +0300

    Some refactoring

--- ngram.py
+++ ngram.py
@@ -1,12 +1,14 @@
-import random
+from random import choices
 import re
 import json
+from collections import defaultdict
+
 
 class NGram():
     def __init__(self, n):
         assert n >= 1, "size of n-gram must be at leat 1"
         self.n = n
-        self.prior = {}
+        self.prior = defaultdict(int)
         self.prob = {}
     
     def save(self, model_file):
@@ -23,47 +25,44 @@ class NGram():
             self.prob = data["prob"]
     
     def train(self, dataset_file):
-        transitions = {}
-        count = {}
+        transitions = defaultdict(list)
+        count = defaultdict(int)
         with open(dataset_file, "r", encoding="utf-8") as fd:
             for line in fd:
-                line = line.strip()
-                words = line.split()
+                words = line.strip().split()
                 B = " ".join(words[:self.n - 1])
-                if not B in self.prior:
-                    self.prior[B] = 0
                 self.prior[B] += 1
                 for i in range(0, len(words) - self.n + 1):
                     B = " ".join(words[i : i + self.n - 1])
                     A = words[i + self.n - 1]
-                    if not B in transitions:
-                        transitions[B] = []
                     if not f"{B} {A}" in count:
                         count[f"{B} {A}"] = 0
-                    if not B in count:
-                        count[B] = 0
                     count[f"{B} {A}"] += 1
                     count[B] += 1
                     if not A in transitions[B]:
                         transitions[B].append(A)
-        for B in transitions.keys():
-            self.prob[B] = {}
-            for A in transitions[B]:
-                self.prob[B][A] = count[f"{B} {A}"] / count[B]
+        self.prob = {
+            B: {
+                A: count[f"{B} {A}"] / count[B]
+                for A in transitions[B]
+            }
+            for B in transitions.keys()
+        }
 
     def generate(self, begin=None, length=100):
+        def randchoice(distribution):
+            return choices(
+                list(distribution.keys()),
+                weights=distribution.values()
+            )
         if begin is None:
-            begin = random.choices(list(self.prior.keys()), weights=self.prior.values())
-            begin = begin[0].split()
-        else:
-            begin = begin[0].split()
+            begin = randchoice(self.prior)
+        begin = begin[0].split()
         res = begin
         for _ in range(length):
             B = " ".join(res[-self.n + 1:])
             if B not in self.prob:
                 break
-            distribution = self.prob[B].values()
-            A = random.choices(list(self.prob[B].keys()), weights=distribution)
-            res += A
+            res += randchoice(self.prob[B])
         return " ".join(res)
 
